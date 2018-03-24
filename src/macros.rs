49e4051e417ae01b3a9a5906a2fa88447d67dce4
comp1 <chotom73@gmail.com>
2018-03-24 13:52:00 +0900

    .

---
+++ src/macros.rs
@@ -0,0 +1,8 @@
+#![macro_escape]
+
+#[macro_export]
+macro_rules! welcome {
+    () => (
+        println!("Welcome to RUST Macro");
+    )
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,59 @@
+struct Robot {
+
+}
+
+impl Robot {
+    fn greet(&self) {
+        println!("Hello, Developer!")
+    }
+}
+/**
+* module
+* items, functions, structs, impls are defined here
+**/
+mod robot1 {
+    /**
+     * private function
+    **/
+    fn say_hello() {
+        println!("Hello")
+    }
+
+    /**
+     * public function
+    **/
+    pub fn say_hi() {
+        println!("Hi")
+    }
+}
+
+mod macros;
+
+
 fn main() {
-    println!("Hello, world!");
+    println!("Hello, robot!");
+    let mut bot = Robot {};
+    bot.greet();
+
+    let mut a = 5;
+    let mut b = 1;
+    while  a == 5 {
+        b += 1;
+        if b == 5 {
+            a += 1;
+        }
+    }
+
+    let x = 0;
+    for x in 0..10 {
+        println!("{}", x);
+    }
+
+    //robot1::say_hello();
+    robot1::say_hi();
+    // function alias
+    use robot1::say_hi as hi;
+    hi();
+
+    welcome!();
 }
